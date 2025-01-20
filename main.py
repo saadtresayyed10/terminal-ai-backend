0bc9c90ae21d73452f792ee48aff213df829677e
unknown <saadsyed950@gmail.com>
2025-01-20 19:04:57 +0530

    AI function defined

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ load_dotenv()
 
 apiKey = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=apiKey)
+
+print("/t/t/t/tConnected To AI Server/t/t/t/t")
+
+def chatWithBot(prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt)
+    
+    if response and hasattr(response, "text"):
+        return response.text.strip()
+    return "Sorry, failed to understand this..."
