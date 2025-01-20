c78f25037c7e56838f8e88ee024c2e525d2d9d38
unknown <saadsyed950@gmail.com>
2025-01-20 19:33:43 +0530

    Function to read the ai file defined

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/run-script", methods=["GET"])
+def runScript():
+    try:
+        command = ["python3", "main.py"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        return result.stdout if result.returncode == 0 else f"Error: {result.stderr}", 200
+    except Exception as e:
+        return f"Failed to run the script {str(e)}", 500
+
+
+         
